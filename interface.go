a09b1e13e431be3bad4083a604f02aaa5f167e28
Jakub Sztandera <kubuxu@protonmail.ch>
2016-11-21 18:44:53 +0100

    Add bit of docs

--- interface.go
+++ interface.go
@@ -9,6 +9,10 @@ import (
 
 const envKey = "LIBP2P_FORCE_PNET"
 
+// Setting this variable to true or setting LIBP2P_FORCE_PNET environment variable
+// to true will make libp2p to require private network protector.
+// If no network protector is provided and this variable is set to true libp2p will
+// refuse to connect.
 var ForcePrivateNetwork bool = false
 
 func init() {
@@ -18,6 +22,9 @@ func init() {
 var ErrNotInPrivateNetwork = errors.New("private network was not configured but" +
 	" is enforced by the environment")
 
+// This interface is a way for private network implementation to be transparent in
+// libp2p. It is created by implementation and use by libp2p-conn to secure connections
+// so they can be only established with selected number of peers.
 type Protector interface {
 	Protect(iconn.Conn) (iconn.Conn, error)
 }
